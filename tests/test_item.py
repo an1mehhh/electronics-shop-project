c9d05c0f954b3e25c5d8e973ff33c0608a71485c
an1meh <adolf.ddd@mail.ru>
2023-08-08 19:05:04 +0300

    add basic functionality

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,17 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+
+from src.item import Item
+import pytest
+
+
+@pytest.fixture
+def item():
+    return Item("Смартфон", 10000, 20)
+
+
+def test_calculate_total_price(item):
+    assert item.calculate_total_price() == 200000
+
+
+def test_apply_discount(item):
+    assert item.apply_discount() == item.price
